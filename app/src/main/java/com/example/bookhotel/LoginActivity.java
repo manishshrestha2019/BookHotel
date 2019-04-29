8fdd2241dabf0c254303bc2bf6476a157f80bc45
Manish Shrestha <manish.sh2071@gmail.com>
2019-04-29 09:53:48 +0545

    improved

--- app/src/main/java/com/example/bookhotel/LoginActivity.java
+++ app/src/main/java/com/example/bookhotel/LoginActivity.java
@@ -1,24 +1,13 @@
 package com.example.bookhotel;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity  implements View.OnClickListener  {
-    private Button btnSignup;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnSignup= findViewById(R.id.btnSignup);
-
-        btnSignup.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v){
-        setContentView(R.layout.activity_signup);
     }
 }
--- app/src/main/java/com/example/bookhotel/SignupActivity.java
+++ app/src/main/java/com/example/bookhotel/SignupActivity.java
@@ -2,16 +2,12 @@ package com.example.bookhotel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_signup);
     }
 }
